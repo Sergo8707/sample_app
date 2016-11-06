4c596e37613a949b350650ae0443e9a6085c8dd4
sergo <sergo8707@gmail.com>
2016-11-06 16:40:41 +0200

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,10 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def signed_in?
     !current_user.nil?
   end
@@ -24,5 +28,15 @@ module SessionsHelper
     current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+
+  # Код реализующий дружественную переадресацию.
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,35 +1,59 @@
 require 'spec_helper'
 
-describe "Static pages" do
+# тесты для страницы со списком пользователей.
+describe "User pages" do
 
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
+  describe "index" do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+      visit users_path
+    end
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    it "should list each user" do
+      User.all.each do |user|
+        expect(page).to have_selector('li', text: user.name)
+      end
+    end
   end
 
-  describe "Help page" do
-    before { visit help_path }
+  describe "Static pages" do
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
-  end
+    subject { page }
 
-  describe "About page" do
-    before { visit about_path }
+    describe "Home page" do
+      before { visit root_path }
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
-  end
+      it { should have_content('Sample App') }
+      it { should have_title(full_title('')) }
+      it { should_not have_title('| Home') }
+    end
 
-  describe "Contact page" do
-    before { visit contact_path }
+    describe "Help page" do
+      before { visit help_path }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+      it { should have_content('Help') }
+      it { should have_title(full_title('Help')) }
+    end
+
+    describe "About page" do
+      before { visit about_path }
+
+      it { should have_content('About') }
+      it { should have_title(full_title('About Us')) }
+    end
+
+    describe "Contact page" do
+      before { visit contact_path }
+
+      it { should have_content('Contact') }
+      it { should have_title(full_title('Contact')) }
+    end
   end
 end
